e8b060181e968cf15ac4c8766c5511f10120a29d
vuxinfix05 <nguyentruongvu000111@gmail.com>
2025-07-24 22:15:53 +0700

    completed 24/7/2025 10:15pm

--- lesson34.js
+++ lesson34.js
@@ -1,6 +1,7 @@
 let requestUserById = async (id) => {
     let promise = (await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`))
-    if(promise.status == 404) throw new Error("Not found!")
+    if (promise.status == 404) throw new Error("Not found!")
+    if (promise.status !== 200) throw new Error("Status !== 200")
     let jsonData = await promise.json()
     //callback
     console.log(jsonData);
@@ -9,4 +10,9 @@ let requestUserById = async (id) => {
 
 requestUserById("ss")
     .then(data => console.log("ahihi"))
-    .catch(err => console.log("baka"))
+    .catch(err => {
+        if (err instanceof Error)
+            console.log(err.message, "baka")
+    })
+
+//try - catch(e)
